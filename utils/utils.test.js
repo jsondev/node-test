07ca43fc11d37d3d80dba9c34b9b48760a01b814
James Anderson <jsondev1.0@outlook.com>
2018-04-09 10:32:35 -0400

    adding expect and testing functionality

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,8 +1,22 @@
 const utils = require('./utils');
+const expect = require('expect');
 
 it('should add two numbers', () => {
     var res = utils.add(33, 11);
-    if (res !== 44) {
-        throw new Error(`Expected 44, but got ${res} instead.`);
-    }
+    expect(res).toBe(44).toBeA('number');
+    // if (res !== 44) {
+    //     throw new Error(`Expected 44, but got ${res} instead.`);
+    // }
+});
+
+it('should square one number', ()=>{
+    var res = utils.square(2);
+    expect(res).toBe(4).toBeA('number');
+    // if (res !== 4){
+    //     throw new Error (`Expected 4, but got ${res} instead.`);
+    // }
+});
+
+it('should expect some values', ()=>{
+    expect(12).toNotBe(13);
 });
